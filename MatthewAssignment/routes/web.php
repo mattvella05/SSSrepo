aeccc2492ab671fb6fb40c543029256eedaad429
Matthew <matthew.vella14@gmail.com>
2025-04-02 16:01:27 +0000

    Added explicit routes for colleges and students

--- MatthewAssignment/routes/web.php
+++ MatthewAssignment/routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CollegeController;
+use App\Http\Controllers\StudentController;
 
 /*
-|--------------------------------------------------------------------------
+|----------------------------------------------------------------------
 | Web Routes
-|--------------------------------------------------------------------------
+|----------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
@@ -16,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Resource routes for colleges and students
+Route::resource('colleges', CollegeController::class);
+Route::resource('students', StudentController::class);
+
+// Optional filtering route for students by college
+Route::get('students/filter', [StudentController::class, 'filter'])->name('students.filter');
