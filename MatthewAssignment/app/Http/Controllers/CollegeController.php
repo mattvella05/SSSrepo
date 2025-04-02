0d245cf79f7b101d6b4fa9f621b0259acb9d532f
Matthew <matthew.vella14@gmail.com>
2025-04-02 16:09:08 +0000

    Created CollegeController and StudentController with CRUD methods

---
+++ MatthewAssignment/app/Http/Controllers/CollegeController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\College;
+use Illuminate\Http\Request;
+
+class CollegeController extends Controller
+{
+    public function index()
+    {
+        $colleges = College::all();
+        return view('colleges.index', compact('colleges'));
+    }
+
+    public function create()
+    {
+        return view('colleges.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|unique:colleges',
+            'address' => 'required',
+        ]);
+
+        College::create($request->all());
+
+        return redirect()->route('colleges.index');
+    }
+
+    public function edit(College $college)
+    {
+        return view('colleges.edit', compact('college'));
+    }
+
+    public function update(Request $request, College $college)
+    {
+        $request->validate([
+            'name' => 'required|unique:colleges,name,' . $college->id,
+            'address' => 'required',
+        ]);
+
+        $college->update($request->all());
+
+        return redirect()->route('colleges.index');
+    }
+
+    public function destroy(College $college)
+    {
+        $college->delete();
+
+        return redirect()->route('colleges.index');
+    }
+}
