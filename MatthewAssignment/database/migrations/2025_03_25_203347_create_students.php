7f8dc83a4bcd19fa498fec1644099fc9ecacf58a
Matthew <matthew.vella14@gmail.com>
2025-04-02 15:40:35 +0000

    Database setup finished

--- MatthewAssignment/database/migrations/2025_03_25_203347_create_students.php
+++ MatthewAssignment/database/migrations/2025_03_25_203347_create_students.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->date('dob');
+            $table->foreignId('college_id')->constrained()->onDelete('cascade'); // Foreign key to colleges
             $table->timestamps();
         });
     }
